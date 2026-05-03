4d2bf5014c465546aba209d312f9d0d64abad879
dougduanne <doug.duanne@gmail.com>
2026-05-03 16:32:31 -0400

    changed ip to use router

--- Laptop Dashboard/web/components/video-panel.js
+++ Laptop Dashboard/web/components/video-panel.js
@@ -30,7 +30,7 @@ class VideoPanel extends HTMLElement {
         
         // Start both streams dynamically
         // Note: Make sure the IP matches your actual robot IP!
-        const host = "100.97.255.110:9002"; 
+        const host = "192.168.1.50:9002"; 
         this.startStreaming('1', `http://${host}/video/1.h264`);
         this.startStreaming('2', `http://${host}/video/2.h264`);
     }
