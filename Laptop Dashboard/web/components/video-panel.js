bdccacecc004d9b3550dc91fc3a8ee9adee6b0ce
dougduanne <doug.duanne@gmail.com>
2026-05-03 15:05:29 -0400

    Pushing this version because it works on the router with my laptop. Triggers do not work -- need to merge in firefox branch

--- Laptop Dashboard/web/components/video-panel.js
+++ Laptop Dashboard/web/components/video-panel.js
@@ -30,7 +30,9 @@ class VideoPanel extends HTMLElement {
         
         // Start both streams dynamically
         // Note: Make sure the IP matches your actual robot IP!
-        const host = "100.97.255.110:9002"; 
+        // Pi Tailscale IP 100.97.255.110
+        // Pi router IP 192.168.1.50
+        const host = "192.168.1.50:9002";
         this.startStreaming('1', `http://${host}/video/1.h264`);
         this.startStreaming('2', `http://${host}/video/2.h264`);
     }
