89581c70dd7d31216a3bce74afea3f6d743ac652
dougduanne <doug.duanne@gmail.com>
2026-05-05 12:39:16 -0400

    fixed triggers on firefox. both controllers now work to control the whole rover.

--- Laptop Dashboard/web/components/video-panel.js
+++ Laptop Dashboard/web/components/video-panel.js
@@ -52,7 +52,7 @@ class VideoPanel extends HTMLElement {
         });
 
         try {
-            const response = await fetch('http://192.168.0.60:9002/video.h264', {
+            const response = await fetch('http://100.97.255.110:9002/video.h264', {
                 signal: this.abortController.signal
             });
             
