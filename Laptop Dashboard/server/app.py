c497ca77855da357443a1914106061d63891d0f8
dougduanne <doug.duanne@gmail.com>
2026-05-05 11:07:44 -0400

    Revert "literally just added comments and changed to tailscale"
    This reverts commit e16ac1f28f5bb5d2cb32c6bef262e53feba74e38.

--- Laptop Dashboard/server/app.py
+++ Laptop Dashboard/server/app.py
@@ -33,10 +33,7 @@ from aiohttp import web, WSMsgType, ClientSession, ClientTimeout
 STATIC_DIR = pathlib.Path(__file__).parent.parent / "web"
 
 # Point this to your Pi
-# Pi Tailscale: 100.97.255.110
-# Pi Router: 192.168.1.50
-PI_HTTP_VIDEO = "http://100.97.255.110:9002/video.mjpg"
-
+PI_HTTP_VIDEO = "http://192.168.1.50:9002/video.mjpg"   # <-- change IP
 # The Pi connects IN to the laptop WS (so no PI_WS_URL needed here)
 
 class DriverStationServer:
